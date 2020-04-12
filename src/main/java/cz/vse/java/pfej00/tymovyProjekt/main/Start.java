d1c9a27dee77d121a9f5526ce848fe6f51972a40
Jan Pfeiffer <jan.pfeiffer@seznam.cz>
2020-04-12 19:42:53 +0200

    Initial commit - basic maven project with controller and fxml

---
+++ src/main/java/cz/vse/java/pfej00/tymovyProjekt/main/Start.java
@@ -0,0 +1,36 @@
+package cz.vse.java.pfej00.tymovyProjekt.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+
+public class Start extends Application
+{
+    /**
+     * Metoda, prostřednictvím níž se spouští celá aplikace.
+     *
+     * @param args Parametry příkazového řádku
+     */
+    public static void main(String[] args)
+    {
+        launch();
+    }
+
+    /**
+     * Metoda pro vyvolání GUI
+     * @param primaryStage
+     * @throws Exception
+     */
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+        primaryStage.setTitle("Projekt");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+}
